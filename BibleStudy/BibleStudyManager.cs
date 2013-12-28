94e21a41a5317b152f561f973693e293cb125854
John Welty <ajw1970@gmail.com>
2013-12-28 15:37:54 -0600

    Refactoring BibleStudyManager

--- BibleStudy/BibleStudyManager.cs
+++ BibleStudy/BibleStudyManager.cs
@@ -9,8 +9,7 @@ namespace BibleStudy
     public abstract class BibleStudyManager
     {
         private Binder bible;
-        private List<BookData> books;
-        protected BibleReader reader;
+        protected List<BookData> books;
         protected static BibleStudyManager bibleStudyManager;
         private string userId;
 
@@ -21,7 +20,6 @@ namespace BibleStudy
         {
             bible = Service.GetBible();
             books = Service.GetCannonizedBookData();
-            reader = new BibleReader(books, new ReadingListData());
         }
 
         protected ReadingChapter ConvertHeaderToChapter(ReadingChapterHeader header)
@@ -43,47 +41,6 @@ namespace BibleStudy
                 Number = header.Number,
                 Verses = verses,
             };
-        }
-    }
-
-    public class MockBibleStudyManager : BibleStudyManager
-    {
-        public static BibleStudyManager Instance
-        {
-            get
-            {
-                if (bibleStudyManager == null)
-                    bibleStudyManager = new MockBibleStudyManager();
-                return bibleStudyManager;
-            }
-        }
-
-        private MockBibleStudyManager() 
-        {
-            reader.AddReadingList("Gen", "Deut", "Ex", 11);
-            reader.AddReadingList("Joshua", "2 Chron", "Ruth", 1);
-            reader.AddReadingList("Ezra", "Job", "Ezra", 3);
-            reader.AddReadingList("Psalm", 47);
-            reader.AddReadingList("Prov", "Song", "Prov", 25);
-            reader.AddReadingList("Isaiah", "Daniel", "Jer", 9);
-            reader.AddReadingList("Hosea", "Malachi", "Micah", 9);
-            reader.AddReadingList("Matt", "John", "Matt", 7);
-            reader.AddReadingList("Acts", "2 Cor", "1 Cor", 8);
-            reader.AddReadingList("Gal", "Rev", "Titus", 3);
-            reader.SetCurrentListIndex(1);
-        }
-
-        public override ReadingChapter CurrentChapter
-        {
-            get
-            {
-                return ConvertHeaderToChapter(reader.CurrentChapterHeader);
-            }
-        }
-
-        public override ReadingChapter GetNextChapter()
-        {
-            return ConvertHeaderToChapter(reader.NextChapterHeader);
         }
     }
 }
---
+++ BibleStudy/MockBibleStudyManager.cs
@@ -0,0 +1,52 @@
+﻿using BibleModel;
+using KjvBible;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BibleStudy
+{
+    public class MockBibleStudyManager : BibleStudyManager
+    {
+        private BibleReader reader;
+
+        public static BibleStudyManager Instance
+        {
+            get
+            {
+                if (bibleStudyManager == null)
+                    bibleStudyManager = new MockBibleStudyManager();
+                return bibleStudyManager;
+            }
+        }
+
+        private MockBibleStudyManager()
+        {
+            reader = new BibleReader(books, new ReadingListData());
+            reader.AddReadingList("Gen", "Deut", "Ex", 11);
+            reader.AddReadingList("Joshua", "2 Chron", "Ruth", 1);
+            reader.AddReadingList("Ezra", "Job", "Ezra", 3);
+            reader.AddReadingList("Psalm", 47);
+            reader.AddReadingList("Prov", "Song", "Prov", 25);
+            reader.AddReadingList("Isaiah", "Daniel", "Jer", 9);
+            reader.AddReadingList("Hosea", "Malachi", "Micah", 9);
+            reader.AddReadingList("Matt", "John", "Matt", 7);
+            reader.AddReadingList("Acts", "2 Cor", "1 Cor", 8);
+            reader.AddReadingList("Gal", "Rev", "Titus", 3);
+            reader.SetCurrentListIndex(1);
+        }
+
+        public override ReadingChapter CurrentChapter
+        {
+            get
+            {
+                return ConvertHeaderToChapter(reader.CurrentChapterHeader);
+            }
+        }
+
+        public override ReadingChapter GetNextChapter()
+        {
+            return ConvertHeaderToChapter(reader.NextChapterHeader);
+        }
+    }
+}
