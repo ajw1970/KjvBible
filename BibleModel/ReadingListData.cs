8ac61e54e25afb25d66a1130e693c6209a4344d1
John Welty <ajw1970@gmail.com>
2013-12-29 15:31:33 -0600

    Refactoring

---
+++ BibleModel/ReadingListData.cs
@@ -0,0 +1,27 @@
+﻿using BibleModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleModel
+{
+    public class ReadingListData
+    {
+        public int CurrentListIndex { get; set; }
+        public List<ReadingList> Lists { get; set; }
+
+        public ReadingListData()
+        {
+            Lists = new List<ReadingList>();
+        }
+    }
+
+    public class ReadingList
+    {
+        public string Name { get; set; }
+        public List<ReadingChapterHeader> ReadingChapters { get; set; }
+        public int CurrentChapterIndex { get; set; }
+    }
+}
