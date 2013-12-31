47355d13f5e34e471e83d7449b83027295673c3a
John Welty <ajw1970@gmail.com>
2013-12-31 12:10:35 -0600

    Brought in DbContext from the S8W Web to reference from here

---
+++ BibleContext/BibleReaderDataContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibleModel;
+using BibleStudy;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace BibleContext
+{
+    public class BibleReaderDataContext : ReadingListDataAccessor
+    {
+        ReadingListContext db;
+
+        public BibleReaderDataContext()
+        {
+            db = new ReadingListContext();
+        }
+
+        public void SaveReadingListData(string userName, ReadingListData data)
+        {
+            var jsonData = JsonConvert.SerializeObject(data);
+            var graph = getGraph(userName);
+
+            if (graph == null)
+            {
+                graph = new ReadingListObjectGraph
+                {
+                    UserName = userName,
+                    Data = jsonData
+                };
+                db.ReadingListObjectGraphs.Add(graph);
+            }
+            else
+            {
+                graph.Data = jsonData;
+            }
+            db.SaveChanges();
+        }
+
+        public ReadingListData LoadReadingListData(string userName)
+        {
+            var jsonData = getGraph(userName).Data;
+            var data = JsonConvert.DeserializeObject<ReadingListData>(jsonData);
+            return data;
+        }
+
+        private ReadingListObjectGraph getGraph(string userName)
+        {
+            return db.ReadingListObjectGraphs.Where(d => d.UserName == userName).FirstOrDefault();
+        }
+    }
+
+    public class ReadingListContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public ReadingListContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<ReadingListObjectGraph> ReadingListObjectGraphs { get; set; }
+
+    }
+
+    public class ReadingListObjectGraph
+    {
+        [Key]
+        public string UserName { get; set; }
+        public string Data { get; set; }
+    }
+}
--- BibleStudy/BibleStudyManagerImp.cs
+++ BibleStudy/BibleStudyManagerImp.cs
@@ -47,20 +47,21 @@ namespace BibleStudy
                 var reader = new BibleReader(books, new ReadingListData());
 
                 //todo: need to put this back to a sane default. 
-                //reader.AddReadingList("Gen", "Rev", "Gen", 1);
-                //reader.SetCurrentListIndex(0);
+                reader.AddReadingList("Gen", "Rev", "Gen", 1);
+                reader.SetCurrentListIndex(0);
 
-                reader.AddReadingList("Gen", "Deut", "Ex", 11);
-                reader.AddReadingList("Joshua", "2 Chron", "Ruth", 1);
-                reader.AddReadingList("Ezra", "Job", "Ezra", 3);
-                reader.AddReadingList("Psalm", 47);
-                reader.AddReadingList("Prov", "Song", "Prov", 25);
-                reader.AddReadingList("Isaiah", "Daniel", "Jer", 9);
-                reader.AddReadingList("Hosea", "Malachi", "Micah", 9);
-                reader.AddReadingList("Matt", "John", "Matt", 7);
-                reader.AddReadingList("Acts", "2 Cor", "1 Cor", 8);
-                reader.AddReadingList("Gal", "Rev", "Titus", 3);
-                reader.SetCurrentListIndex(1);
+                //reader.AddReadingList("Gen", "Deut", "Ex", 11);
+                //reader.AddReadingList("Joshua", "2 Chron", "Ruth", 1);
+                //reader.AddReadingList("Ezra", "Job", "Ezra", 3);
+                //reader.AddReadingList("Psalm", 47);
+                //reader.AddReadingList("Prov", "Song", "Prov", 25);
+                //reader.AddReadingList("Isaiah", "Daniel", "Jer", 9);
+                //reader.AddReadingList("Hosea", "Malachi", "Micah", 1);
+                //reader.AddReadingList("Matt", "John", "Matt", 7);
+                //reader.AddReadingList("Acts", "2 Cor", "1 Cor", 8);
+                //reader.AddReadingList("Gal", "Rev", "Titus", 3);
+                //reader.SetCurrentListIndex(1);
+
                 return reader;
             }
         }
